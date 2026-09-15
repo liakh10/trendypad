17a6bb39d52f340a60ab481c56a3d604666d454d
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-16 01:18:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x6748a26fdab5da3AB54cF8458276Ca92acF0713F",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/trendypad_rh",
   NAME:    "Trendypad",
   TICKER:  "$TRENDY",
